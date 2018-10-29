3dad7c9f124cf07c33b317e8269b74f997b57c30
Viktor Godard <viktorgodard@gmail.com>
2018-10-29 04:28:50 +0000

    Example 03C: Turn on LED while button is pressed and keep it on after it is released with improved formula and simple de-bouncing.

---
+++ sketch_03C.ino
@@ -0,0 +1,35 @@
+// Example 03C: Turn on LED while button is pressed
+//              and keep it on after it is released
+//              with improved formula and simple de-bouncing.
+//              20181025
+
+const int LED = 13;
+const int BUTTON = 7;
+
+int val = 0;
+int old_val = 0;
+int state = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = digitalRead(BUTTON);
+  
+  if ((val == HIGH) && (old_val == LOW)) {
+    state = 1 - state;
+    delay(10);
+  }
+
+  old_val = val;
+  
+  if (state == 1) {
+    digitalWrite(LED, HIGH);
+    } else {
+      digitalWrite(LED, LOW);
+    }
+}
