5f7e0353a272efd180083026b4a0ffaa73a3313d
Viktor Godard <viktorgodard@gmail.com>
2018-10-30 01:36:20 +0000

    Vik_Project1: Convert RC Tamiya Unimog to Arduino controlled.

---
+++ vik_project1-20181029.sh
@@ -0,0 +1,24 @@
+# Vik_Project1
+# Convert RC Unimog to Arduino controlled. 
+
+# Specs:
+Item: RC Mercedes-Benz Unimog 406 Series U900 (CC-01)
+Length: 407mm
+Width: 220mm
+Height: 229mm
+Weight: 1460g
+Wheelbase: 242mm
+Tread (Front & Rear): 163mm
+Tire Width/Diameter (Front & Rear): 33/90mm
+
+Chassis: Ladder Frame Bathtub Type
+Drivetrain: Longitudinally Placed Motor Shaft-Driven Full-Time 4WD
+Differential: Front and Rear 3-Bevel Differential Gears
+Front Suspension: Double Wishbone, Rear Suspension: 4-Link Rigid
+Dampers: Front and Rear CVA Oil Dampers
+Gear Radio: Gear Ratio＝1：14.67
+
+Electronic Speed Controller: TEU-104BK ESC Tamiya
+Motor: RS540 Type Tamiya Motor
+Servo: HS-645MG HiTec ultra torque Servo
+
