1a27fae8dc0b0c9a3ef16c0dc49fb4a3b05306af
Viktor Godard <viktorgodard@gmail.com>
2018-10-30 03:01:56 +0000

    added specs for RC vehicle components

--- vik_project1-20181029.sh
+++ vik_project1-20181029.sh
@@ -1,8 +1,10 @@
 # Vik_Project1
 # Convert RC Unimog to Arduino controlled. 
 
-# Specs:
+# General Specs:
 Item: RC Mercedes-Benz Unimog 406 Series U900 (CC-01)
+Scale: 1/10 Electric R/C Car Series No.457
+Link: http://www.tamiya.com/english/products/58457unimog/index.htm
 Length: 407mm
 Width: 220mm
 Height: 229mm
@@ -18,7 +20,65 @@ Front Suspension: Double Wishbone, Rear Suspension: 4-Link Rigid
 Dampers: Front and Rear CVA Oil Dampers
 Gear Radio: Gear Ratio＝1：14.67
 
-Electronic Speed Controller: TEU-104BK ESC Tamiya
-Motor: RS540 Type Tamiya Motor
+ESC: TEU-104BK ESC Tamiya
+Motor: RS540 Type RS-540SH , made by Tamiya Mabuchi Motor Co. LTD
 Servo: HS-645MG HiTec ultra torque Servo
+
+# Electronic Speed Controller Specs:
+ESC: TEU-104BK ESC Tamiya
+Link: https://www.tamiyausa.com/shop/tools/rc-esc-teu-104bk-brushed-2/
+Battery: Lithium-Ion battery only!
+BEC: Warning, this model as no BEC (battery eliminator circuitry) 
+     which is needed by modern RC receivers to regulate and reduce voltage 
+     from 7.2V down to 6V
+Size: 39.4x36.5x15mm. Weight: 47g. 
+Input voltage: 6.6-7.2V.
+Maximum continuous current of 60A forward and 30A reverse.
+Motor support: Sport Tuned Motors (Item 53068) and 25T and above motors.
+Heat and current protection functions with LED indicator.
+Battery voltage cut-off function can be disabled. 
+The setting button allows the forward/reverse and neutral positions to be easily set. 
+The reverse function can be disabled. 
+Features heat and current protection functions with LED indicator. 
+Forward (100%) and reverse (50%) output. 
+Equipped with an aluminum heat sink.
+
+# Motor Specs:
+Motor: RS540 Type RS-540SH-7520 , made by Tamiya Mabuchi Motor Co. LTD
+Link: http://www.alphalanding.com/rc-track/rs_540rhsh.pdf
+Torque: 27 or 25 Turn Tamiya Torque Tuned motor
+Top Speed: 24.3Km/h or 26km/h
+
+# Servo Specs:
+Servo: HS-645MG HiTec ultra torque Servo
+Link: https://www.robotshop.com/media/files/pdf/hs645mg.pdf
+Dimensions:  1.59" x 0.77"x 1.48" (40.6 x 19.8 x 37.8mm)
+Product Weight:  1.94oz. (55.2g)
+Output Shaft Style:  24 tooth (C1) spline
+Voltage Range:  4.8V - 6.0V
+No-Load Speed (4.8V):  0.24sec/60°
+No-Load Speed (6.0V):  0.20sec/60°
+Stall Torque (4.8V):   106.93 oz/in. (7.7kg.cm)
+Stall Torque (6.0V):   133.31 oz/in. (9.6kg.cm)
+Max PWM Signal Range (Standard): 553-2520μsec
+Travel per µs (out of box):  .100°/μsec
+Max Travel (out of box): 197°
+Pulse Amplitude:  3-5V
+Operating Temperature:  -20°C to +60°C
+Current Drain - idle (4.8V):      8.8mA
+Current Drain - idle (6.0V):      9.1mA
+Current Drain - no-load (4.8V): 350mA
+Current Drain - no-load (6V):   450mA
+Continuous Rotation Modifiable: Yes
+Direction w/ Increasing PWM Signal:  Clockwise
+Deadband Width:  8µs
+Motor Type: 3 Pole Ferrite
+Potentiometer Drive: Indirect Drive
+Feedback Style:  5KΩ Potentiometer
+Output Shaft Support:   Dual Ball Bearings
+Gear Type:  Straight Cut Spur
+Gear Material:  3 Metal Gears and 1 Resin Metal Gear
+Wire Length: 11.81" (300mm)
+Wire Gauge:  22AWG
+
 
